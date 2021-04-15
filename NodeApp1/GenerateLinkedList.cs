645c0268a2be8d0bdc3f501414616e6e093b62f3
tobyStupp <40300257+tobyStupp@users.noreply.github.com>
2021-04-15 23:06:38 +0300

    Add project files.

---
+++ NodeApp1/GenerateLinkedList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unit4;
+
+namespace NodeApp1
+{
+    class GenerateLinkedList
+    {
+        public Node<int> GetLinkedList (int count, int range = 20)
+        {
+            Node<int> dummy = new Node<int>(-1);
+            Node<int> last = dummy;
+            Random rnd = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                last.SetNext(new Node<int>(rnd.Next(20) + 1);
+                last = last.GetNext();
+            }
+
+            return dummy.GetNext();
+        }
+        public Node<int> GetLinkedList (int [] values)
+        {
+            Node<int> dummy = new Node<int>(-1);
+            Node<int> last = dummy;
+            foreach (int x in values)
+            {
+                last.SetNext(new Node<int>(x));
+                last = last.GetNext();
+            }
+            return dummy.GetNext();
+        }
+    }
+}
---
+++ NodeApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unit4;
+
+namespace NodeApp1
+{
+    class Program
+    {
+    public static int Count (Node<int> p)
+        {
+            // input: Node<int> representing the first Node
+            // output: how many Nodes exist in Linked List
+            throw new NotImplementedException();
+        }
+        public static int Min (Node<int> p)
+        {
+            // input: Node<int> representing the first Node
+            // output: minimum value in Linked List
+            throw new NotImplementedException();
+        }
+       public static Node<int> GetLast (Node<int> p)
+        {
+            //input: First Node of Linked List
+            // output: the last node of the linked list
+            throw new NotImplementedException();
+        }
+        public static bool Exists (Node<int> p, int x)
+        {
+            
+                // input: Node<int> representing the first Node and value x
+                // output: true if value in Node exists (true) else false
+              
+                throw new NotImplementedException();
+             
+        }
+        public static int CountInList (Node<int> p, int x)
+        {
+            // input: Node<int> representing the first Node and value x
+            // output: value of how many times x exists in linked list that begins with p
+
+            throw new NotImplementedException();
+
+        }
+        public Node<int> GetPlace (Node<int> p, int place)
+        {
+            // input: Node<int> representing the first Node and value place
+            // output: the Node<int> at place place
+
+            throw new NotImplementedException();
+
+        }
+        static void Main(string[] args)
+        {
+          
+            Console.ReadKey();
+        }
+    }
+}
