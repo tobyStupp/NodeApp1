c41963fb727c9bf4e5265a7b60a90f03f9629397
tobyStupp <40300257+tobyStupp@users.noreply.github.com>
2021-04-17 21:39:07 +0300

    corrections

--- NodeApp1/GenerateLinkedList.cs
+++ NodeApp1/GenerateLinkedList.cs
@@ -9,20 +9,20 @@ namespace NodeApp1
 {
     class GenerateLinkedList
     {
-        public Node<int> GetLinkedList (int count, int range = 20)
+        public static Node<int> GetLinkedList (int count, int range = 20)
         {
             Node<int> dummy = new Node<int>(-1);
             Node<int> last = dummy;
             Random rnd = new Random();
             for (int i = 0; i < count; i++)
             {
-                last.SetNext(new Node<int>(rnd.Next(20) + 1);
+                last.SetNext(new Node<int>(rnd.Next(20) + 1));
                 last = last.GetNext();
             }
 
             return dummy.GetNext();
         }
-        public Node<int> GetLinkedList (int [] values)
+        public static Node<int> GetLinkedList (int [] values)
         {
             Node<int> dummy = new Node<int>(-1);
             Node<int> last = dummy;
